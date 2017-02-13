9c010b987055f8562721ece0baa504a701cfd8a7
gness1804 <graham.nessler@gmail.com>
2017-02-13 12:39:07 -0700

    Create index html

--- elevator.js
+++ elevator.js
@@ -1,5 +1,6 @@
 export default class Elevator {
   constructor() {
+    this.currentFloor = 0;
   }
 
   reset() {
--- person.js
+++ person.js
@@ -1,4 +1,7 @@
 export default class Person {
   constructor() {
+    this.name = '';
+    this.currentFloor = null;
+    this.requestedFloor = null;
   }
 }
