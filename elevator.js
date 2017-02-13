246abfe4e5f5c659a63a7bc41808cd1133ce74dc
gness1804 <graham.nessler@gmail.com>
2017-02-13 12:45:28 -0700

    Instantiate Person and Elevator constructors with their properties

--- elevator.js
+++ elevator.js
@@ -1,6 +1,13 @@
 export default class Elevator {
   constructor() {
     this.currentFloor = 0;
+    this.dropOffFloor = null;
+    this.floorsTraversed = 0;
+    this.stopsMade = 0;
+    this.requests = [];
+    this.currentRiders = [];
+    this.status = 'idle';
+    this.direction = '';
   }
 
   reset() {
--- person.js
+++ person.js
@@ -3,5 +3,6 @@ export default class Person {
     this.name = '';
     this.currentFloor = null;
     this.requestedFloor = null;
+    this.weight = null;
   }
 }
